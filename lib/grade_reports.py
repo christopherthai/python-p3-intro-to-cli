bc499c9691aca370dee4a6c6ac2fc75d5482efaf
Christopher Thai <christopherthai.nyli@gmail.com>
2024-04-08 19:09:37 -0400

    complete lab

--- lib/grade_reports.py
+++ lib/grade_reports.py
@@ -1,4 +1,36 @@
 #!/usr/bin/env python3
 
-if __name__ == '__main__':
-    pass
+
+def create_grade_report(student_grades):
+    with open("./reports/grade_report.txt", "w") as gr:
+        for grade in student_grades:
+            # add '\n' to write grades on separate lines
+            gr.write(grade + "\n")
+
+
+if __name__ == "__main__":
+    student_grades = []
+
+    grade = input("Student name, grade: ")
+    while grade:
+        student_grades.append(grade)
+        # end when no grade is entered
+        grade = input("Student name, grade: ")
+
+    create_grade_report(student_grades)
+
+
+# class MyClass:
+#     def __init__(self, user_input):
+#         self.value = user_input
+
+
+# def my_function(my_object):
+#     pass
+#     # returns a final value for the CLI workflow
+
+
+# if __name__ == "__main__":
+#     user_input = input("Enter something here: ")
+#     my_object = MyClass(user_input)
+#     print(my_function(my_object))
